2678fab99c1d90b354f680047078f3cd85684b3b
Jerry Liao <chejui.liao@gmail.com>
2020-07-28 15:43:58 -0400

    add files

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# ocr_engines
-explore different ocr engines and compare them
+
+# OCR engine comparison - Tesseract vs. EasyOCR
+
+This project is exploring different ocr engines and comparing them
+
+This document is for people who want to observe the performance for ocr engines
+
+The codes are running under python3.6 environment
+
+### Step 1: install requirements
+
+open a terminal and run `pip3 install -r requirements.txt`
+
+### Step 2: run python script
+
+In the terminal, run `python3 test_ocr.py <test_size> <test_type>`
+
+e.g. `python3 test_ocr.py 100 number`
+
+`test_size`: how many samples to test, default is `1000`
+
+`test_type`: test on text or number, default is `text`
---
+++ test_ocr.py
@@ -0,0 +1,200 @@
+def create_img(file: str, text):
+    """
+    create image with text on white background
+
+    :param file: the path to save the image
+    :type file: str
+    :param text: the text to write on the image
+    :type text: str or float
+    :return: None
+    :rtype: None
+    """
+    img = Image.new('RGB', (200, 50), color = (255, 255, 255))
+    d = ImageDraw.Draw(img)
+    fnt = ImageFont.truetype('arial.ttf', 15)  # set up font
+    d.text((10,10), str(text), font=fnt, fill=(0,0,0))  # write text on image
+    img.save(file)
+    img.close()
+
+def parse_xml(xml):
+    """
+    parse xml text to be accessed through xpath
+
+    :param xml: xml text
+    :type xml: str
+    :return: parsel.selector.Selector
+    :rtype: object
+    """
+    # setup xml parser
+    parsel.Selector.__str__ = parsel.Selector.extract
+    parsel.Selector.__repr__ = parsel.Selector.__str__
+    parsel.SelectorList.__repr__ = lambda x: '[{}]'.format(
+        '\n '.join("({}) {!r}".format(i, repr(s))
+                   for i, s in enumerate(x, start=1))
+    ).replace(r'\n', '\n')
+
+    doc = parsel.Selector(text=xml)
+    return doc
+
+def tesseract_read(file: str):
+    """
+    extract text from the image using Tesseract
+
+    :param file: path to the image
+    :type file: str
+    :return: text
+    :rtype: str
+    """
+    hocr = pytesseract.image_to_pdf_or_hocr(file, extension='hocr')
+    xml = hocr.decode('utf-8')
+    doc = parse_xml(xml)
+    tsa_output = []
+
+    # get text
+    for tag in doc.xpath('/html/body/div/div/p/span/span'):
+        tsa_output.append(str(tag.xpath('text()')[0]))
+
+    tsa_output = " ".join(tsa_output).lower()  # lowercase the output to be better compared with easyocr
+    tsa_output = tsa_output.replace('-', ' ')  # replace '-' to be better compared with easyocr
+    
+    return tsa_output
+
+def easyocr_read(file: str):
+    """
+    extract text from the image using EasyOCR
+
+    :param file: path to the image
+    :type file: str
+    :return: text
+    :rtype: str
+    """
+    reader = easyocr.Reader(['th','en'], gpu = True)
+    results = reader.readtext(file)
+    results = sorted(results, key=lambda x: x[0][0])
+    text_results = [x[-2] for x in results][0]
+    easy_output = text_results.strip()
+    
+    return easy_output
+
+def compare_diff(df, wrong_column: str, answer_column: str):
+    """
+    compare the differences between two columns from a dataframe
+
+    :param df: dataframe with wrong and answer column
+    :type df: dataframe
+    :param wrong_column: name of wrong column
+    :type wrong_column: str
+    :param answer_column: name of answer column
+    :type answer_column: str
+    :return:
+    :rtype:
+    """
+
+    df = df.copy()
+    df['add'] = ''
+    df['delete'] = ''
+    for idx, row in df.iterrows():
+        a = row[wrong_column]
+        b = row[answer_column]
+        add_list = []
+        delete_list = []
+        # print('{} => {}'.format(a,b))
+        for i,s in enumerate(difflib.ndiff(a, b)):
+            if s[0]==' ': continue
+            elif s[0]=='-':
+                delete_list.append(s[-1])
+                # print(u'Delete "{}"'.format(s[-1]))
+            elif s[0]=='+':
+                add_list.append(s[-1])
+                # print(u'Add "{}"'.format(s[-1]))
+        if len(delete_list) > 0:
+            df.loc[idx, 'delete'] = "|".join(delete_list)
+        if len(add_list) > 0:
+            df.loc[idx, 'add'] = "|".join(add_list)
+
+    return df
+
+if __name__ == '__main__':
+    from random_words import RandomWords
+    from PIL import Image, ImageDraw, ImageFont
+    import random
+    import time
+    import pytesseract
+    import easyocr
+    import parsel
+    import numpy as np
+    import pandas as pd
+    import difflib
+    import sys
+
+    try:
+        test_size = int(sys.argv[1])
+    except:
+        test_size = 100
+    try:
+        test_type = sys.argv[2]  # text or number
+    except:
+        test_type = 'text'
+
+    file = 'test_img.jpg'
+    answer_list = []
+    tsa_list = []
+    easy_list = []
+    tsa_time_used = 0
+    easy_time_used = 0
+    rw = RandomWords()
+    for i in range(test_size):
+        print(i)
+        cont = 1
+        # create data
+        if test_type == 'number':
+            text = str(round(random.uniform(10000, 99999),2))
+        elif test_type == 'text':
+            text = " ".join(rw.random_words(count=2))  # generate two words
+            text = text.replace('-', ' ')  # replace '-' for easyocr
+            if len(text) > 22:  # in case the text is beyond the image size
+                text = text[:22]
+
+        create_img(file, text)  # create the image with text
+        
+        # get result from tesseract
+        tsa_start_time = time.time()
+        tsa_result = tesseract_read(file)
+        tsa_end_time = time.time()
+        tsa_time_used = tsa_time_used + (tsa_end_time-tsa_start_time)/60
+        
+        # get result from easyocr
+        easy_start_time = time.time()
+        easy_result = easyocr_read(file)
+        easy_end_time = time.time()
+        easy_time_used = easy_time_used + (easy_end_time-easy_start_time)/60
+        
+        # append result
+        answer_list.append(text.lower())
+        tsa_list.append(tsa_result)
+        easy_list.append(easy_result)
+
+    # convert to array
+    answer_array = np.array(answer_list)
+    tsa_array = np.array(tsa_list)
+    easy_array = np.array(easy_list)
+
+    # calculate error rates
+    tsa_error_rate = np.mean(answer_array!=tsa_array) * 100
+    easy_error_rate = np.mean(answer_array!=easy_array) * 100
+
+    # print out error rates
+    print(f"Tesseract error rate on {test_size} samples: {tsa_error_rate}%, used {tsa_time_used} minutes")
+    print(f"EasyOCR error rate on {test_size} samples: {easy_error_rate}%, used {easy_time_used} minutes")
+
+    # store wrong cases and answer in dataframe
+    df_tsa_wrong = pd.DataFrame({'tsa_wrong': tsa_array[tsa_array!=answer_array], 'answer': answer_array[tsa_array!=answer_array]})
+    df_easy_wrong = pd.DataFrame({'easy_wrong': easy_array[easy_array!=answer_array], 'answer': answer_array[easy_array!=answer_array]})
+
+    df_tsa_wrong_diff = compare_diff(df_tsa_wrong, 'tsa_wrong', 'answer')
+    df_easy_wrong_diff = compare_diff(df_easy_wrong, 'easy_wrong', 'answer')
+
+    # output dataframe as csv
+    df_tsa_wrong.to_csv('tsa_wrong_diff.csv')
+    df_easy_wrong.to_csv('easy_wrong_diff.csv')
+
